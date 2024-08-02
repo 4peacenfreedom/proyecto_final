bc5f4c4322b097a5ca07d8b760c7ce73df6466d0
4peacenfreedom <4peacenfreedom@gmail.com>
2024-08-01 20:10:03 -0600

    entregable 3 - import/export

--- js/CrearHTML.js
+++ js/CrearHTML.js
@@ -1,3 +1,11 @@
+/* 0- */
+export function MostrarHTML() {
+  let navbar = (document.getElementById("Navbar").innerHTML = CrearNavbar());
+  let footer = (document.getElementById("Footer").innerHTML = CrearFooter());
+  let Noticias = (document.getElementById("Noticias").innerHTML =
+    CrearTarjetaNoticias());
+}
+
 /* 1-Navbar */
 function CrearNavbar() {
   let html = `
--- js/Main.js
+++ js/Main.js
@@ -1,13 +1,6 @@
-console.log("Funciona");
+import { MostrarHTML } from "/js/CrearHTML.js";
 
 /* Los componentes */
 document.addEventListener("DOMContentLoaded", (ev) => {
   MostrarHTML();
 });
-/* 0- */
-function MostrarHTML() {
-  let navbar = (document.getElementById("Navbar").innerHTML = CrearNavbar());
-  let footer = (document.getElementById("Footer").innerHTML = CrearFooter());
-  let Noticias = (document.getElementById("Noticias").innerHTML =
-    CrearTarjetaNoticias());
-}
