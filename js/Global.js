f3cd45c8a23a4ac857e5ca3e0e6eb0159b785ec0
4peacenfreedom <4peacenfreedom@gmail.com>
2024-08-06 20:18:10 -0600

    entregable 3 - Agosto 6

--- js/Global.js
+++ js/Global.js
@@ -3,8 +3,6 @@
 export function MostrarHTML() {
   let navbar = (document.getElementById("Navbar").innerHTML = CrearNavbar());
   let footer = (document.getElementById("Footer").innerHTML = CrearFooter());
-  let Noticias = (document.getElementById("Noticias").innerHTML =
-    CrearTarjetaNoticias());
 }
 
 /* 1-Navbar */
